63ebd514bc9fbddbaca3682340c0613dafcda3ce
Dmitry Sukhikh <coddeys@gmail.com>
2015-11-17 15:52:52 -0200

    Create redis suggestions_for sets, merge sets analytics with suggestions_for and then return this set as questions json.

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,13 +8,23 @@ class Question
     "How do I signup" ]
 
   def self.suggestions_for(query)
-    @redis.zrevrange "analytics", 0, 10
+    @redis.del "out"
+    @redis.zinterstore("out", ["analytics", "suggestions_for:#{query.downcase}"])
+    @redis.zrange("out", 0, -1)
   end
 
   def self.index_questions
     @questions.each do |q|
       @redis.zincrby "analytics", 0, q
+      index_suggestion q, q
+      q.split.each { |k| index_suggestion(k, q) }
     end
   end
 
+  def self.index_suggestion(key,value)
+    1.upto(key.length) do |n|
+      prefix = key[0,n]
+      @redis.sadd "suggestions_for:#{prefix.downcase}", value
+    end
+  end
 end
